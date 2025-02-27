b31a72cf9c5ab9315915f3dc45e2b15ca9927ba7
KarlTilsner <karltilsner@icloud.com>
2025-02-27 23:05:23 +1030

    Added Firebase and Firestore

---
+++ JS/init-firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAuIloyHiVem2ZoC9AZSyO_033Cb5jZtBs",
+    authDomain: "karl-tilsner.firebaseapp.com",
+    projectId: "karl-tilsner",
+    storageBucket: "karl-tilsner.firebasestorage.app",
+    messagingSenderId: "721661094361",
+    appId: "1:721661094361:web:ca1561f333b066213511c8"
+};
+
+firebase.initializeApp(firebaseConfig);
