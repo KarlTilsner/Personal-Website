8d2841352c8b593cc5ecf8410d3c293983f55be3
KarlTilsner <karltilsner@icloud.com>
2025-03-07 22:11:43 +1030

    Can now retrieve images from Cloudinary

--- react-app/src/ProjectImages.tsx
+++ react-app/src/ProjectImages.tsx
@@ -1,19 +1,15 @@
 import { Cloudinary } from "@cloudinary/url-gen";
-import { auto } from "@cloudinary/url-gen/actions/resize";
-import { autoGravity } from "@cloudinary/url-gen/qualifiers/gravity";
 import { AdvancedImage } from "@cloudinary/react";
+import { fill } from "@cloudinary/url-gen/actions/resize";
 
 const ProjectImages = () => {
-    const cld = new Cloudinary({ cloud: { cloudName: "dq70ltuo8" } });
+    const cld = new Cloudinary({
+        cloud: { cloudName: "dq70ltuo8" },
+    });
 
-    // Use this sample image or upload your own via the Media Explorer
-    const img = cld
-        .image("cld-sample-5")
-        .format("auto") // Optimize delivery by resizing and applying auto-format and auto-quality
-        .quality("auto")
-        .resize(auto().gravity(autoGravity()).width(500).height(500)); // Transform the image: auto-crop to square aspect_ratio
+    const myImage = cld.image("qijobpdiwngoj1lb2rh8").resize(fill().width(500));
 
-    return <AdvancedImage cldImg={img} />;
+    return <AdvancedImage cldImg={myImage} />;
 };
 
 export default ProjectImages;
